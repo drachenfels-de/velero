589578ad58528a6bdb1ce88d23b7af786e734cb8
Ruben Jenster <r.jenster@drachenfels.de>
2023-08-23 17:48:41 +0000

    WIP build.sh

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -eu
+
+export BUILDX_PLATFORMS=linux/amd64
+make container
+
+img=localhost/velero/velero:main
+gitver="$(git describe --tags)"
+dest="harbor.intern.drachenfels.de/user.ruben/velero:$gitver"
+buildah push $img "$dest"
+
+echo "$gitver"
+echo "$dest"
